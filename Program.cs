960ec30f352ef11e18d2a294b0f3b895a897c463
MylkShake <danil_lp@live.com>
2016-10-15 23:53:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace KappaLeagends_Loader
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(System.EventArgs args)
+        {
+            KappaLeagends.Init.Initialize();
+        }
+    }
+}
